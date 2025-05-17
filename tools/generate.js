7983b3be4a26eb15dbb142fe6d91a488d091e4fa
cervantesx <jeff@vrodex.com>
2025-05-17 16:11:20 +0200

    Typescript Support

--- tools/generate.js
+++ tools/generate.js
@@ -1,128 +1,125 @@
-import fs from 'node:fs';
-import { stringify } from 'javascript-stringify';
-
+import fs from "node:fs";
+import { stringify } from "javascript-stringify";
 
 function generatePrinters() {
-    let output = '';
-    output += `const printerDefinitions = {\n`;
+  let output = "";
+  output += `const printerDefinitions = {\n`;
 
+  try {
+    let files = fs.readdirSync("data/printers");
 
-    try {
-        let files = fs.readdirSync('data/printers');
+    for (let file of files) {
+      let data = fs.readFileSync("data/printers/" + file, "utf8");
 
-        for (let file of files) {
-            let data = fs.readFileSync('data/printers/' + file, 'utf8');
-            let printer;
+      let definition = {};
 
-            let definition = {};
+      try {
+        definition = JSON.parse(data);
+      } catch (err) {
+        console.log("Error parsing file: " + file);
+        console.log(data);
+        throw err;
+      }
 
-            try {
-                definition = JSON.parse(data);
-            }
-            catch (err) {
-                console.log('Error parsing file: ' + file);
-                console.log(data);
-                throw err;
-            }
+      let name = file.replace(/\.json$/, "");
 
-            let name = file.replace(/\.json$/, '');
-
-            output += `\t'${name}': ${stringify(definition)},\n`;
-        }
+      output += `\t'${name}': ${stringify(definition)},\n`;
     }
-    catch (err) {
-        console.error(err);
-    }
+  } catch (err) {
+    console.error(err);
+  }
 
-    output += '};\n\n';
+  output += "};\n\n";
 
-    output += 'export default printerDefinitions;\n';
+  output += "export default printerDefinitions;\n";
 
-    fs.writeFileSync('generated/printers.js', output, 'utf8');
+  fs.writeFileSync("generated/printers.js", output, "utf8");
 }
 
-
 function generateMappings() {
+  let output = "";
+  output += `const codepageMappings = {\n`;
 
-    let output = '';
-    output += `const codepageMappings = {\n`;
+  try {
+    output += `\t'esc-pos': {\n`;
 
+    let files = fs.readdirSync("data/mappings/esc-pos");
 
-    try {
-        output += `\t'esc-pos': {\n`;
+    for (let file of files) {
+      let data = fs.readFileSync("data/mappings/esc-pos/" + file, "utf8");
+      let lines = data.split("\n");
 
-        let files = fs.readdirSync('data/mappings/esc-pos');
+      let name = file.replace(/\.txt$/, "").replace(/-legacy/g, "/legacy");
+      let list = new Map();
 
-        for (let file of files) {
-            let data = fs.readFileSync('data/mappings/esc-pos/' + file, 'utf8');
-            let lines = data.split("\n");
-
-            let name = file.replace(/\.txt$/, '').replace(/-legacy/g, '\/legacy');
-            let list = new Map();
-
-            for (let line of lines) {
-                if (line.length > 1 && line.charAt(0) != '#') {
-                    let [ skip, key, value ] = line.split(/\t/);
-                    list.set(parseInt(key, 16), value.trim());
-                }
-            }
-
-            let mapping = new Array(Math.max(...list.keys()));
-
-            for (let [ key, value ] of list) {
-                mapping[key] = value;
-            }   
-
-            output += `\t\t'${name}': ${stringify(mapping)},\n`;
+      for (let line of lines) {
+        if (line.length > 1 && line.charAt(0) != "#") {
+          let parts = line.split(/\t/).filter((part) => part.length > 0);
+          if (parts.length >= 2) {
+            let key = parts[0];
+            let value = parts[1];
+            list.set(parseInt(key, 16), value.trim());
+          }
         }
+      }
 
-        output += `\t},\n`;
+      let mapping = [];
+      for (let value of list.values()) {
+        mapping.push(value);
+      }
+
+      output += `\t\t'${name}': ${stringify(mapping)},\n`;
     }
-    catch (err) {
-        console.error(err);
-    }
 
-    try {
-        output += `\t'star-prnt': {\n`;
+    output += `\t},\n`;
+  } catch (err) {
+    console.error(err);
+  }
 
-        let files = fs.readdirSync('data/mappings/star-prnt');
+  try {
+    output += `\t'star-prnt': {\n`;
 
-        for (let file of files) {
-            let data = fs.readFileSync('data/mappings/star-prnt/' + file, 'utf8');
-            let lines = data.split("\n");
+    let files = fs.readdirSync("data/mappings/star-prnt");
 
-            let name = file.replace(/\.txt$/, '').replace(/-legacy/g, '\/legacy');
-            let list = new Map();
+    for (let file of files) {
+      let data = fs.readFileSync("data/mappings/star-prnt/" + file, "utf8");
+      let lines = data.split("\n");
 
-            for (let line of lines) {
-                if (line.length > 1 && line.charAt(0) != '#') {
-                    let [ skip, key, value ] = line.split(/\t/);
-                    list.set(parseInt(key, 16), value.trim());
-                }
-            }
+      let name = file.replace(/\.txt$/, "").replace(/-legacy/g, "/legacy");
+      let list = new Map();
 
-            let mapping = new Array(Math.max(...list.keys()));
-
-            for (let [ key, value ] of list) {
-                mapping[key] = value;
-            }   
-
-            output += `\t\t'${name}': ${stringify(mapping)},\n`;
+      for (let line of lines) {
+        if (line.length > 1 && line.charAt(0) != "#") {
+          let parts = line.split(/\t/).filter((part) => part.length > 0);
+          if (parts.length >= 2) {
+            let key = parts[0];
+            let value = parts[1];
+            list.set(parseInt(key, 16), value.trim());
+          }
         }
+      }
 
-        output += '\t}\n';
+      let mapping = [];
+      for (let value of list.values()) {
+        mapping.push(value);
+      }
+
+      output += `\t\t'${name}': ${stringify(mapping)},\n`;
     }
-    catch (err) {
-        console.error(err);
-    }
 
-    output += '};\n\n';
+    output += "\t}\n";
+  } catch (err) {
+    console.error(err);
+  }
 
-    output += 'codepageMappings[\'star-line\'] = codepageMappings[\'star-prnt\'];\n';
-    output += 'codepageMappings[\'esc-pos\'][\'zijang\'] = codepageMappings[\'esc-pos\'][\'pos-5890\'];\n\n';
-    output += 'export default codepageMappings;\n';
+  output += "};\n\n";
 
-    fs.writeFileSync('generated/mapping.js', output, 'utf8');
+  output += "codepageMappings['star-line'] = codepageMappings['star-prnt'];\n";
+  output +=
+    "codepageMappings['esc-pos']['zijang'] = codepageMappings['esc-pos']['pos-5890'];\n\n";
+  output += "export default codepageMappings;\n";
+
+  fs.writeFileSync("generated/mapping.js", output, "utf8");
 }
 
 generateMappings();
